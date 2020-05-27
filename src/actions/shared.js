ef679f74f1253620eee728112b0ef29343c022f7
BertoGz <AlbertoG@albertos-mbp.attlocal.net>
2020-05-27 16:25:41 -0700

    setup the actions in the actions folders

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,9 +1,16 @@
 import {getInitialData} from '../utils/api'
+import {recieveUsersAction} from './users'
+import {recieveTweetsAction} from './tweets'
+import {setAuthedUserAction} from './authedUser'
+
+const AUTHED_ID='sarah_edo'
 
 export funtion handleInitialData(){
 	return (dispatch)=>{
 		return getInitialData().then((users,tweets)=>{
-			
+			dispatch(recieveUsersAction(users))
+			dispatch(recieveTweetsAction(tweets))
+			dispatch(setAuthedUserAction(AUTHED_ID))
 		})
 	}
 }
--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,6 +1,8 @@
- export const RECIEVE_TWEETS = 'RECIEVE_TWEETS'
+export const RECIEVE_TWEETS = 'RECIEVE_TWEETS'
 
- export function recieveTweetsAction(tweets){
+
+
+export function recieveTweetsAction(tweets){
  	return{
  		type: RECIEVE_TWEETS,
  		tweets
