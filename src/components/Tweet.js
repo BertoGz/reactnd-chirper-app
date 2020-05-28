cd9faa1c8b429a33f3b6202a4a5134fae0216e59
BertoGz <AlbertoG@albertos-mbp.attlocal.net>
2020-05-28 15:49:47 -0700

    added a checker to the tweet component, if tweet doesnt exist, dont render the tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,6 +5,12 @@ import {formatTweet} from '../utils/helpers'
 
 class Tweet extends Component{
 	render(){
+		const {tweet} = this.props
+
+		if (tweet===null){
+			return <p>this tweet doesnt exist</p>
+		}
+
 		console.log(this.props)
 		return(
 			<div className="tweet">
@@ -17,11 +23,12 @@ class Tweet extends Component{
 // we will grab these states into this componentes props
 function mapStateToProps({authedUserReducer,usersReducer,tweetsReducer},{id}){
 	const tweet = tweetsReducer[id];
-	
+	const parentTweet = tweet ? tweetsReducer[tweet.replyingTo] : null  // if tweet is a thing return tweet
 
 	return{
 		authedUserReducer, 
-		tweet: formatTweet(tweet, usersReducer[tweet.author], authedUserReducer)
+		tweet: tweet ? formatTweet(tweet, usersReducer[tweet.author], authedUserReducer, parentTweet)
+		: null  // if tweet is a thing return tweet
 	}
 }
 
