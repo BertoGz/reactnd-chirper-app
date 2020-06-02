1142a1037a803826bd0b41548ddb97751e2ff00e
BertoGz <AlbertoG@albertos-mbp.attlocal.net>
2020-06-01 17:04:36 -0700

    the tweet page now shows a tweet. we can reply to a tweet.

--- src/components/TweetPage.js
+++ src/components/TweetPage.js
@@ -1,14 +1,28 @@
 import React,{Component} from 'react'
 import {connect} from 'react-redux'
 
+import Tweet from './Tweet'
+import NewTweet from './NewTweet'
 class TweetPage extends Component{
 
 	render(){
 
-		console.log(this.props)
+		const {id, replies} = this.props
 		return(
 			<div>
-				TWEET PAGE
+
+				<Tweet id={id}/> {/*renders the main tweet*/}
+				<NewTweet id={id}/> {/*renders the create new tweet component, if there is a valid id passed, it will reply*/}
+				{replies.length!==0 && 
+					<h3 className='center'>Replies</h3>
+				}
+				<ul>
+					{replies.map((replyId)=>(
+						<li key={replyId}>
+							<Tweet id={replyId}/>
+						</li>
+					))}
+				</ul>
 			</div>
 		)
 	}
